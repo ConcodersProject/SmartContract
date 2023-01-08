9a020741af624e2ffd6dcd6a6b7ce4f5d9b48398
ankushKun <ankush4singh@gmail.com>
2023-01-08 11:00:23 +0530

    buy, transfer

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use core::fmt::Debug;
 #[derive(Serialize, SchemaType, Clone)]
 pub struct ItemInput {
     pub name: String,
-    pub price: u64,
+    pub price: Amount,
     pub total_supply: u64,
     pub image_url: String,
 }
@@ -13,7 +13,7 @@ pub struct ItemInput {
 #[derive(Serialize, SchemaType, Clone)]
 pub struct Item {
     pub name: String,
-    pub price: u64,
+    pub price: Amount,
     pub total_supply: u64,
     pub image_url: String,
 
@@ -37,6 +37,12 @@ enum Error {
     /// Failed parsing the parameter.
     #[from(ParseError)]
     ParseParamsError,
+    /// Errors
+    NameLengthError,
+    TotalSupplyError,
+    ImageUrlError,
+    ItemNotFoundError,
+    ItemNotOwnedError,
 }
 
 /// Init function that creates a new smart contract.
@@ -45,8 +51,6 @@ fn init<S: HasStateApi>(
     _ctx: &impl HasInitContext,
     _state_builder: &mut StateBuilder<S>,
 ) -> InitResult<State<S>> {
-    // Your code
-
     Ok(State {
         items: _state_builder.new_map(),
         item_count: 0,
@@ -61,12 +65,13 @@ type NewItemData = ItemInput;
     name = "add_item",
     parameter = "ItemInput",
     error = "Error",
+    return_value = "u64",
     mutable
 )]
 fn add_item<S: HasStateApi>(
     ctx: &impl HasReceiveContext,
     host: &mut impl HasHost<State<S>, StateApiType = S>,
-) -> Result<(), Error> {
+) -> Result<u64, Error> {
     // Your code
 
     let input: NewItemData = ctx.parameter_cursor().get()?;
@@ -81,24 +86,112 @@ fn add_item<S: HasStateApi>(
     };
 
     let state = host.state_mut();
+    ensure!(item.name.len() > 0, Error::NameLengthError);
+    ensure!(item.total_supply > 0, Error::TotalSupplyError);
+    ensure!(item.image_url.len() > 0, Error::ImageUrlError);
+
     // ensure!(
     //     ctx.sender().matches_account(&ctx.owner()),
     //     Error::OwnerError
     // );
 
-    state.items.insert(0, item);
-    Ok(())
+    state.items.insert(state.item_count, item);
+    state.item_count += 1;
+    Ok(state.item_count - 1)
 }
+
+#[receive(
+    contract = "market",
+    name = "buy_item",
+    parameter = "Vec<u64>",
+    error = "Error",
+    return_value = "Vec<(u64,bool)>",
+    mutable,
+    payable
+)]
+fn buy_item<S: HasStateApi>(
+    ctx: &impl HasReceiveContext,
+    host: &mut impl HasHost<State<S>, StateApiType = S>,
+    amount: Amount,
+) -> Result<Vec<(u64, bool)>, Error> {
+    // Your code
+    let mut return_value: Vec<(u64, bool)> = Vec::new();
+    let input: Vec<u64> = ctx.parameter_cursor().get()?;
+    let mut amt: Amount = amount;
+    for item_id in input {
+        let state = host.state_mut();
+        let mut item = state
+            .items
+            .get_mut(&item_id)
+            .ok_or(Error::ItemNotFoundError)?;
+        if item.sold < item.total_supply {
+            // Items available
+            if item.price <= amount {
+                // Sent enough balance
+                item.owners.push(ctx.sender());
+                item.sold += 1;
+                return_value.push((item_id, true));
+                amt -= item.price;
+            } else {
+                return_value.push((item_id, false));
+            }
+        } else {
+            return_value.push((item_id, false));
+        }
+        item.sold += 1;
+        item.owners.push(ctx.sender());
+        return_value.push((item_id, true));
+    }
+
+    // ensure!(
+    //     ctx.sender().matches_account(&ctx.owner()),
+    //     Error::OwnerError
+    // );
+
+    Ok(return_value)
+}
+
+#[receive(
+    contract = "market",
+    name = "transfer_item",
+    parameter = "(u64, Address)",
+    error = "Error",
+    return_value = "bool",
+    mutable
+)]
+fn transfer_item<S: HasStateApi>(
+    ctx: &impl HasReceiveContext,
+    host: &mut impl HasHost<State<S>, StateApiType = S>,
+) -> Result<bool, Error> {
+    // Your code
+    let input: (u64, Address) = ctx.parameter_cursor().get()?;
+    let state = host.state_mut();
+    let mut item = state
+        .items
+        .get_mut(&input.0)
+        .ok_or(Error::ItemNotFoundError)?;
+    ensure!(
+        item.owners.contains(&ctx.sender()),
+        Error::ItemNotOwnedError
+    );
+    item.owners.push(input.1);
+    let index = item.owners.iter().position(|x| *x == ctx.sender()).unwrap();
+    item.owners.swap_remove(index);
+
+    Ok(true)
+}
+
+// Read only views
 
 type ViewItems = Vec<u64>;
 /// View function that returns the items vector of the item indexes we want
 #[receive(
     contract = "market",
-    name = "view",
+    name = "get_items",
     parameter = "Vec<u64>",
     return_value = "Vec<Option<Item>>"
 )]
-fn view<'a, 'b, S: HasStateApi>(
+fn get_items<'a, 'b, S: HasStateApi>(
     _ctx: &'a impl HasReceiveContext,
     host: &'b impl HasHost<State<S>, StateApiType = S>,
 ) -> ReceiveResult<Vec<Option<Item>>> {
@@ -110,4 +203,14 @@ fn view<'a, 'b, S: HasStateApi>(
         return_items.push(item.as_deref().cloned());
     }
     Ok(return_items)
+}
+
+// Get number of items created
+#[receive(contract = "market", name = "get_item_count", return_value = "u64")]
+fn get_item_count<'a, 'b, S: HasStateApi>(
+    _ctx: &'a impl HasReceiveContext,
+    host: &'b impl HasHost<State<S>, StateApiType = S>,
+) -> ReceiveResult<u64> {
+    let state = host.state();
+    Ok(state.item_count)
 }
